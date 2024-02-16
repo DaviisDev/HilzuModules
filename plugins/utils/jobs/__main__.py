35e6120f663b9afc71c894d4a37d721c3980f377
kannaxuserbot@gmail.com <esliarafernanda@gmail.com>
2024-02-16 14:01:50 -0300

    Tests

--- plugins/utils/jobs/__main__.py
+++ plugins/utils/jobs/__main__.py
@@ -7,7 +7,7 @@ async def gtofertas_(m: Message):
     if m.from_user.id != 1715384854:
         return
     try:
-        id, time, max_messages = m.filtered_input_str.split("|", maxsplit=1)
+        id, time, max_messages = m.filtered_input_str.split("|")
     except ValueError as err:
         return await m.edit(f"Low Arguments!\n\n<code>{err}</code>")
     message_reply = int(id)
